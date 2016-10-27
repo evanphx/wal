ed12436d42d0a0d224b51ca3995a23f58817c8d8
Ryan Uber <ru@ryanuber.com>
2016-10-27 16:57:58 -0700

    Make paired reader/writer sets thread safe

--- pair_test.go
+++ pair_test.go
@@ -1,9 +1,12 @@
 package wal
 
 import (
+	"bytes"
+	"crypto/rand"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
 	"testing"
 	"time"
 
@@ -71,6 +74,57 @@ func TestPair(t *testing.T) {
 		require.NoError(t, r.BlockingNext())
 
 		assert.Equal(t, []byte("data1"), r.Value())
+	})
+
+	n.It("linearizes reads and writes", func() {
+		r, w, err := NewPair(path)
+		require.NoError(t, err)
+
+		// Create a ton of input messages to write
+		in := make([][]byte, 10240)
+		for i := 0; i < len(in); i++ {
+			in[i] = make([]byte, 128)
+			_, err := rand.Read(in[i])
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		// Write all of the messages in the background
+		go func() {
+			defer wg.Done()
+			for _, p := range in {
+				if err := w.Write(p); err != nil {
+					t.Fatal(err)
+				}
+			}
+		}()
+
+		// In a separate goroutine, try reading a bunch of messages.
+		// Since we can't really tell how fast the writer is going,
+		// we'll just make sure that the reads we do get line up
+		// correctly with what we expect.
+		go func() {
+			defer wg.Done()
+			for i := 0; i < len(in); i++ {
+				if !r.Next() {
+					if err := r.Error(); err != nil {
+						t.Fatal(err)
+					}
+					i--
+					continue
+				}
+
+				if !bytes.Equal(r.Value(), in[i]) {
+					t.Fatal("mismach found, probably a race: %v")
+				}
+			}
+		}()
+
+		wg.Wait()
 	})
 
 	n.Meow()
